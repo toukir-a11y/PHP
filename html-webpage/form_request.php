91c496e1a93ded648b3580357384578185949025
toukir <toukirhossain18@gmail.com>
2022-02-23 00:28:41 +0600

    get post method and make a small project

---
+++ html-webpage/form_request.php
@@ -0,0 +1,74 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.css">
+
+    <style>
+       body{
+           margin-top:50px;
+       }
+    </style>
+    
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="colum colum-60 colum-offset-20">
+                <h2>Hello WPPOOLIAN</h2>
+                <p>Just for test purpose</p>
+
+                <p style = "color: red;">
+
+                                                            
+                    <?php
+                        if(isset($_REQUEST['fname']) && !empty($_REQUEST['fname']))
+                        {
+                            printf("%s %s", 'Fname:', $_REQUEST['fname']);
+                        }
+
+                        echo "</br>  </br>";
+
+                        if(isset($_REQUEST['lname']) && !empty($_REQUEST['lname']))
+                        {
+                            printf("%s %s", 'Lname:', $_REQUEST['lname']);
+                        }
+                    ?>
+                </p>                
+            </div>
+        </div>
+    <div class="row">
+        <div class="colum colum-60 colum-offset-20">
+            <form method ="REQUEST">
+                <label for="fname">First name</label><br>
+                <input type="text" id="fname" name="fname">
+                
+                <label for="lname">Last name</label><br>
+                <input type="text" id="lname" name="lname">
+
+                <button type='submit'>submit</button>
+            </form>
+        </div>
+    </div>
+</div>
+</body>
+</html>
+
+
+<?php // GET methon, Post method its a HTTP VERB
+
+      // get methon using for data read 
+
+      // post method using for data save, create  and update 
+
+      // Patch method using for data update but not the full data set some data update in full data set 
+
+      // delete method using for deleting data set or data 
+?>
