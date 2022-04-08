c0b971b7fc1b6df5e5753a668783d32a78705680
toukir <toukirhossain18@gmail.com>
2022-04-09 01:05:24 +0600

    start work with crud project

---
+++ crud/inc/template/nav.php
@@ -0,0 +1,7 @@
+<p>
+
+    <a href="/crud/index.php?task=report">All Students||</a>
+    <a href="/crud/index.php?task=add">Add New Student||</a>
+    <a href="/crud/index.php?task=seed">Seed</a>
+
+</p>
---
+++ crud/index.php
@@ -0,0 +1,32 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>CRUD Project</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.css">
+
+    <style>
+       body{
+           margin-top:50px;
+       }
+    </style>
+    
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="colum colum-60 colum-offset-20">
+                <h2>Crud Project </h2>
+                <p>A simple project to perform crud operation using plain files and php </p>
+                <?php include_once('inc/template/nav.php'); ?>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
